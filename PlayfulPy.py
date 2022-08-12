b00459bee9ef8fa39b638f39f366260076bd7f9e
ataprantzis3 <ataprantzis3@gatech.edu>
2022-08-12 20:02:21 +0100

    Big update: Option to use Spotify Connect to control music. Option to send notification with the name of the currently playing song

--- PlayfulPy.py
+++ PlayfulPy.py
@@ -1,10 +1,34 @@
+from time import sleep
 import rumps
 import threading
 from functions import *
 import configparser
+from multiprocessing import Process
+from flask import Flask, request
+import webbrowser
+import requests
+import base64
+
 
 config = configparser.ConfigParser()
 config.read('PlayfulPy.ini')
+client_id = config['PlayfulPy']['client_id']
+client_secret = config['PlayfulPy']['client_secret']
+redirect_uri = config['PlayfulPy']['redirect_uri']
+uri_port = config['PlayfulPy']['uri_port']
+refresh_token = config['PlayfulPy']['refresh_token'] if 'refresh_token' in config['PlayfulPy'] else None
+server_instance = None
+code=None
+spot_instance = None
+auth_instance = None
+timer = None
+access_token = None
+
+scope = [
+    "user-read-currently-playing",
+    "user-read-playback-state",
+    "user-modify-playback-state",
+];
 
 def formatTitle(song, artist, length):
     if(length=='Long'):    
@@ -32,12 +56,96 @@ def formatTitle(song, artist, length):
                 song_edited = song[0:(26-len(artist)-2)]+'..'
         return song_edited + " - " + artist_edited
 
-class AwesomeStatusBarApp(rumps.App):
+def Server():
+        app = Flask("MyServer")
+        webbrowser.open(('''https://accounts.spotify.com/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope={scope}''').format(client_id=client_id, redirect_uri=redirect_uri, scope=",".join(scope)))
+        
+        @app.before_first_request
+        def before_first_request():
+            print("Opened window")
+        
+        def shutdown_server():
+            func = request.environ.get('werkzeug.server.shutdown')
+            if func is None:
+                raise RuntimeError('Not running with the Werkzeug Server')
+            func()
+
+        @app.after_request
+        def after_request(res):
+            shutdown_server()
+            return res
+
+        @app.route("/")
+        def getCode():
+            global code
+            code = request.args['code']
+            print(request.args['code'])
+            return "<p>Hello, code: !</p> " + request.args['code']
+        app.run(host='localhost', port=config['PlayfulPy']['uri_port'])
+
+def getAccessToken():
+    res = requests.post("https://accounts.spotify.com/api/token", headers={
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization":
+        "Basic " +
+        base64.b64encode((client_id + ":" + client_secret).encode("ascii")).decode('ascii'),
+    },
+    params={
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": redirect_uri
+    })
+    res = res.json()
+    global access_token
+    global refresh_token
+    global timer        
+    access_token=res['access_token']
+    refresh_token=res['refresh_token']
+
+    timer=res['expires_in']
+def getRefreshToken():
+    res = requests.post("https://accounts.spotify.com/api/token", headers={
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization":
+        "Basic " +
+        base64.b64encode((client_id + ":" + client_secret).encode("ascii")).decode('ascii'),
+    },
+    params={
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token,
+    })
+    res = res.json()
+    global access_token
+    global timer
+    access_token=res['access_token']
+    timer=res['expires_in']
+
+def handleSignIn():
+    try:
+        if(refresh_token==None):
+            print("handling server")
+            Server()
+            getAccessToken()
+        else:
+            try:
+                getRefreshToken()
+            except:
+                print("handling server")
+                Server()
+                getAccessToken()
+        PlayfulPy.source = 'connect'
+        
+    except Exception as e:
+        print('Error while signing in')
+        print(e)
+
+class PlayfulPy(rumps.App):
     length = 'Short'
     current_song=["",""]
-    
+    send_notification = False
+    source = 'spotify'  # spotify | connect
     def __init__(self):
-        super(AwesomeStatusBarApp, self).__init__(
+        super(PlayfulPy, self).__init__(
             name="Playful",
             title="Now Playing",
             icon="icons/spotify.png",
@@ -46,80 +154,153 @@ class AwesomeStatusBarApp(rumps.App):
                 'Next',
                 'Previous',
                 None,
-                {'Text Length':
-                    ['Short', 'Long']
+                {'Options':
+                    [{
+                        'Text Length':['Short', 'Long'],
+                        'Source': ['Spotify App', 'Spotify Connect (Experimental)'],
+                        'Send Notification On Change' : ['On', 'Off']
+                    },
+                    ]
                 }
             ],
             quit_button=None
         )
+        
         try:
-            self.length = config['PlayfulPy']['length']
+            if('send_notification' in config['PlayfulPy']):
+                PlayfulPy.send_notification = config['PlayfulPy']['send_notification']=='True'
+            if('length' in config['PlayfulPy']):
+                PlayfulPy.length = config['PlayfulPy']['length']
+            if('source' in config['PlayfulPy']):
+                PlayfulPy.source = config['PlayfulPy']['source']
         except:
-            self.length = 'Short'
+            pass
         def refresh_title():
-            if(isRunning() == 'running'):
+            # print(PlayfulPy.source)
+            # print(PlayfulPy.current_song)
+            if(PlayfulPy.source=='spotify'):
+                if(isRunning() == 'running'):
+                    try:
+                        track = getCurrentTrack()
+                        # print(track)
+                        # print(" and ")
+                        # print(self.current_song)
+                        if(track[0]!=PlayfulPy.current_song[0] or track[1]!=PlayfulPy.current_song[1]):
+                            # print("copying"x)
+                            if(PlayfulPy.send_notification==True):
+                                PlayfulPy.send_notification and rumps.notification(title=track[0], message=track[1], subtitle="")
+                            PlayfulPy.current_song=track.copy()
+                            self.title = formatTitle(track[0], track[1], PlayfulPy.length)
+                            
+                    except:
+                        PlayfulPy.current_song = ['', '']
+                        self.title = "Error"
+                else:
+                    PlayfulPy.current_song = ['', '']
+                    self.title = "Open Spotify"
+            else:
                 try:
-                    track = getCurrentTrack()
+                    track = getCurrentTrack(access_token)
                     # print(track)
                     # print(" and ")
                     # print(self.current_song)
-                    if(track[0]!=self.current_song[0] or track[1]!=self.current_song[1]):
+                    if(track[0]!=PlayfulPy.current_song[0] or track[1]!=PlayfulPy.current_song[1]):
                         # print("copying")
-                        self.current_song=track.copy()
-                        self.title = formatTitle(track[0], track[1], self.length)
-                except:
+                        if(PlayfulPy.send_notification==True):
+                            rumps.notification(title=track[0], message=track[1], subtitle="")
+                        PlayfulPy.current_song=track.copy()
+                        self.title = formatTitle(track[0], track[1], PlayfulPy.length)
+                except Exception as e:
                     self.current_song = ['', '']
-                    self.title = "Error"
-            else:
-                self.current_song = ['', '']
-                self.title = "Open Spotify"
-            threading.Timer(4.0, refresh_title).start()
+                    self.title = "Could Not Get Song"
+                    print(e)
+            threading.Timer(2.0, refresh_title).start()
         refresh_title()
+        
+    @rumps.clicked("Options", "Source", "Spotify Connect (Experimental)")
+    def spotify_app(self, _):
+        x = threading.Thread(target=handleSignIn)
+        x.start()
+    
+    @rumps.clicked("Options", "Source", "Spotify App")
+    def spotify_connect(self, _):
+        PlayfulPy.source="spotify"
+        
+    @rumps.clicked("Options", "Send Notification On Change", "On")
+    def send_notification_on(self, _):
+        PlayfulPy.send_notification = True
+    
+    @rumps.clicked("Options", "Send Notification On Change", "Off")
+    def send_notification_off(self, _):
+        PlayfulPy.send_notification = False
 
     @rumps.clicked("Play / Pause")
     def toggle(self, _):
-        if(getState()=="playing"):
-            pause()
+        if(PlayfulPy.source=='spotify'):
+            if(getState()=="playing"):
+                pause()
+            else:
+                play()
         else:
-            play()
+            if(getState(access_token)=='playing'):
+                pause(access_token)
+            else:
+                play(access_token)
 
     @rumps.clicked("Next")
     def skip_next(self, _):
-        next()
+        if(PlayfulPy.source=='spotify'):
+            next()
+        else:
+            next(access_token)
         self.refresh_title_once()
 
     @rumps.clicked("Previous")
     def skip_prev(self, _):
-        previous()
+        if(PlayfulPy.source=='spotify'):
+            previous()
+        else:
+            previous(access_token)
         self.refresh_title_once()
     
     
-    @rumps.clicked("Text Length", 'Short')
+    @rumps.clicked("Options", "Text Length", 'Short')
     def format_length_short(self, _):
-        self.length='Short'
+        PlayfulPy.length='Short'
         self.refresh_title_once()
     
-    @rumps.clicked("Text Length", 'Long')
+    @rumps.clicked("Options", "Text Length", 'Long')
     def format_length_long(self, _):
-        self.length='Long'
+        PlayfulPy.length='Long'
         self.refresh_title_once()
     
     @rumps.clicked('Quit')
     def save_on_quit(self, _):
-        config['PlayfulPy'] = {
-            'length': self.length
-        }
-        with open('PlayfulPy.ini', 'w') as configfile:
+        config['PlayfulPy']['length'] = PlayfulPy.length
+        config['PlayfulPy']['send_notification'] = str(PlayfulPy.send_notification)
+        config['PlayfulPy']['source'] = PlayfulPy.source
+        if(refresh_token!=None):
+            config['PlayfulPy']['refresh_token'] = refresh_token
+        with open('PlayfulPy.ini', 'w+') as configfile:
             config.write(configfile)
         rumps.quit_application()
     
     def refresh_title_once(self):
+        if(PlayfulPy.source=='spotify'):
             try:
-                track = getCurrentTrack();
-                self.current_song=track.copy()
-                self.title = formatTitle(track[0], track[1], self.length)
+                track = getCurrentTrack()
+                PlayfulPy.current_song=track.copy()
+                self.title = formatTitle(track[0], track[1], PlayfulPy.length)
             except:
                 pass
+        else:
+            try:
+                track = getCurrentTrack(access_token)
+                PlayfulPy.current_song=track.copy()
+                self.title = formatTitle(track[0], track[1], PlayfulPy.length)
+            except:
+                pass
+            
     
 if __name__ == "__main__":
-    AwesomeStatusBarApp().run()
+    PlayfulPy().run()
--- functions.py
+++ functions.py
@@ -1,34 +1,89 @@
+from os import access
 import subprocess
+import requests
 
-def play():
-    subprocess.run(["osascript", 'compiledFunctions/play.scpt'])
+def play(access_token=None):
+    if(access_token==None):
+        subprocess.run(["osascript", 'compiledFunctions/play.scpt'])
+    else:
+        requests.put(
+            url="https://api.spotify.com/v1/me/player/play",
+            headers= {
+                "Authorization": "Bearer " + access_token,
+            }
+        )
 
-def pause():
-    subprocess.run(["osascript", 'compiledFunctions/pause.scpt'])
+def pause(access_token=None):
+    if(access_token==None):
+        subprocess.run(["osascript", 'compiledFunctions/pause.scpt'])
+    else:
+        requests.put(
+            url="https://api.spotify.com/v1/me/player/pause",
+            headers= {
+                "Authorization": "Bearer " + access_token,
+            }
+        )
     
-def next():
-    subprocess.run(["osascript", 'compiledFunctions/next.scpt'])
+def next(access_token=None):
+    if(access_token==None):
+        subprocess.run(["osascript", 'compiledFunctions/next.scpt'])
+    else:
+        requests.post(
+            url="https://api.spotify.com/v1/me/player/next",
+            headers= {
+                "Authorization": "Bearer " + access_token,
+            }
+        )
     
-def previous():
-    subprocess.run(["osascript", 'compiledFunctions/previous.scpt'])
+def previous(access_token=None):
+    if(access_token==None):
+        subprocess.run(["osascript", 'compiledFunctions/previous.scpt'])
+    else:
+        requests.post(
+            url="https://api.spotify.com/v1/me/player/previous",
+            headers= {
+                "Authorization": "Bearer " + access_token,
+            }
+        )
 
-def getState():
-    result = subprocess.run(
-        ["osascript",'compiledFunctions/state.scpt'],
-        capture_output=True,
-        encoding="utf-8",
-    )
-    info = list(map(str.strip, result.stdout.split(",")))
-    return info[0]
+def getState(access_token=None):
+    if(access_token==None):
+        result = subprocess.run(
+            ["osascript",'compiledFunctions/state.scpt'],
+            capture_output=True,
+            encoding="utf-8",
+        )
+        info = list(map(str.strip, result.stdout.split("+")))
+        return info[0]
+    else:
+        res = requests.get(
+            url="https://api.spotify.com/v1/me/player",
+            headers= {
+                "Authorization": "Bearer " + access_token,
+            }
+        ).json()
+        if(res['is_playing']):
+            return 'playing'
+        else:
+            return 'paused'
 
-def getCurrentTrack():
-    result = subprocess.run(
-        ["osascript", 'compiledFunctions/currentTrack.scpt'],
-        capture_output=True,
-        encoding="utf-8",
-    )
-    info = list(map(str.strip, result.stdout.split(",")))
-    return info
+def getCurrentTrack(access_token=None):
+    if(access_token==None):
+        result = subprocess.run(
+            ["osascript", 'compiledFunctions/currentTrack.scpt'],
+            capture_output=True,
+            encoding="utf-8",
+        )
+        info = list(map(str.strip, result.stdout.split("+")))
+        return info
+    else:
+        res = requests.get(
+            url="https://api.spotify.com/v1/me/player",
+            headers= {
+                "Authorization": "Bearer " + access_token,
+            }
+        ).json()
+        return [res['item']['name'], res['item']['artists'][0]['name']]
 
 def isRunning():
     result = subprocess.run(
@@ -36,5 +91,5 @@ def isRunning():
         capture_output=True,
         encoding="utf-8",
     )
-    info = list(map(str.strip, result.stdout.split(",")))
+    info = list(map(str.strip, result.stdout.split("+")))
     return info[0]
